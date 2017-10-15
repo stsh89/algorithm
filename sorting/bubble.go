8455a86a761a5b887ff5d368a8ef9951abf7c0da
Stanislav Shandyga <stanislavshandyga@gmail.com>
2017-10-15 09:41:47 +0300

    Improve bubble sort

--- sorting/bubble.go
+++ sorting/bubble.go
@@ -5,14 +5,10 @@ func Bubble(numbers []int) {
 		return
 	}
 
-	for i := 0; i < len(numbers); i++ {
-		for j := 0; j < len(numbers); j++ {
-			if i == j {
-				continue
-			}
-
-			if numbers[i] < numbers[j] {
-				numbers[i], numbers[j] = numbers[j], numbers[i]
+	for i := 1; i < len(numbers); i++ {
+		for j := len(numbers) - 1; j >= i; j-- {
+			if numbers[j] < numbers[j-1] {
+				numbers[j], numbers[j-1] = numbers[j-1], numbers[j]
 			}
 		}
 	}
