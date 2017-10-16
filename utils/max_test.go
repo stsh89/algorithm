4959d1f7b2a6298f4dcb63cfd2e5feb8ba1b4b48
Stanislav Shandyga <stanislavshandyga@gmail.com>
2017-10-16 13:54:47 +0300

    Add max util

---
+++ utils/max_test.go
@@ -0,0 +1,28 @@
+package utils_test
+
+import (
+	"github.com/stsh89/algorithm/utils"
+	"testing"
+)
+
+var Max = utils.Max
+
+func MaxTest1(t *testing.T) {
+	a, b := 1, 3
+	got := Max(a,b)
+	want := 3
+
+	if got != want {
+		t.Errorf("Max(%d, %d), got %d, want %d", a, b, got, want)
+	}
+}
+
+func MaxTest2(t *testing.T) {
+	a, b := 1, 1
+	got := Max(a,b)
+	want := 1
+
+	if got != want {
+		t.Errorf("Max(%d, %d), got %d, want %d", a, b, got, want)
+	}
+}
