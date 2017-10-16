9d10012c5e5f040c85ee331c1ddf658bc3900e8b
Stanislav Shandyga <stanislavshandyga@gmail.com>
2017-10-16 13:59:34 +0300

    Add Min util

---
+++ utils/min_test.go
@@ -0,0 +1,28 @@
+package utils_test
+
+import (
+	"github.com/stsh89/algorithm/utils"
+	"testing"
+)
+
+var Min = utils.Min
+
+func MinTest1(t *testing.T) {
+	a, b := 1, 3
+	got := Min(a,b)
+	want := 1
+
+	if got != want {
+		t.Errorf("Min(%d, %d), got %d, want %d", a, b, got, want)
+	}
+}
+
+func MinTest2(t *testing.T) {
+	a, b := 1, 1
+	got := Min(a,b)
+	want := 1
+
+	if got != want {
+		t.Errorf("Min(%d, %d), got %d, want %d", a, b, got, want)
+	}
+}
