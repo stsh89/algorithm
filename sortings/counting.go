db4b87f4d0389cca80f432b543e0512316f32beb
Stanislav Shandyga <stanislavshandyga@gmail.com>
2017-10-28 16:16:37 +0300

    Add counting sort

---
+++ sortings/counting.go
@@ -0,0 +1,44 @@
+//
+// COUNTING-SORT
+// 	let C[0..k] be a new array
+// 	for i = 0 to k
+// 		C[i] = 0
+// 	for j = 1 to A.length
+// 		C[A[j]] = C[A[j]] + i, so C[i] contains number of elements equal to i
+// 	for i = 1 to k
+// 		C[i] = C[i] + C[i-1], so C[i] contains number of elements less or equal to i
+// 	for j = A.length down to 1
+// 		B[C[A[j]]] = A[j]
+// 		C[A[j]] = C[A[j]] - 1
+//
+
+package sortings
+
+func Counting(numbers []int, maxNumber int) []int {
+        if len(numbers) < 2 {
+                return numbers
+        }
+
+        res := make([]int, len(numbers))
+        k := maxNumber + 1
+        c := make([]int, k)
+
+        for i := 0; i < k; i++ {
+                c[i] = 0
+        }
+
+        for j := 0; j < len(numbers); j++ {
+                c[numbers[j]]++
+        }
+
+        for i := 1; i < k; i++ {
+                c[i] += c[i-1]
+        }
+
+        for j := len(numbers) -1 ; j >= 0; j-- {
+                res[c[numbers[j]] - 1] = numbers[j]
+                c[numbers[j]]--
+        }
+
+        return res
+}
---
+++ sortings/counting_test.go
@@ -0,0 +1,29 @@
+package sortings_test
+
+import (
+	"github.com/stsh89/algorithm/sortings"
+	"reflect"
+	"testing"
+)
+
+var sortingsTestSet = []struct {
+	in  []int
+	out []int
+        maxNumber int
+}{
+	{[]int{}, []int{}, 0},
+	{[]int{1}, []int{1}, 1},
+	{[]int{2, 1}, []int{1, 2}, 2},
+	{[]int{7, 6, 5, 4, 3, 2, 1}, []int{1, 2, 3, 4, 5, 6, 7}, 7},
+	{[]int{1, 1, 1, 1, 1, 1, 1}, []int{1, 1, 1, 1, 1, 1, 1}, 1},
+}
+
+func TestCounting(t *testing.T) {
+	for _, tt := range sortingsTestSet {
+		got := sortings.Counting(tt.in, tt.maxNumber)
+
+		if !reflect.DeepEqual(got, tt.out) {
+			t.Errorf("Counting(), got %v, want %v", got, tt.out)
+		}
+	}
+}
