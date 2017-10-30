e107f07a4023487af49e149e7baa9edc1bb7cf41
Stanislav Shandyga <ssh@anadeainc.com>
2017-10-30 18:07:38 +0200

    Add counting by digit number sort

--- sortings/counting_by_digit_number.go
+++ sortings/counting_by_digit_number.go
@@ -1,5 +1,39 @@
 package sortings
 
+import (
+	"math"
+)
+
 func CountingByDigitNumber(numbers []int, maxNumber, digitNumber int) []int {
-	return numbers
+	if len(numbers) < 2 {
+		return numbers
+	}
+
+	res := make([]int, len(numbers))
+	k := maxNumber + 1
+	c := make([]int, k)
+
+	for i := 0; i < k; i++ {
+		c[i] = 0
+	}
+
+	for j := 0; j < len(numbers); j++ {
+		c[digit(numbers[j], digitNumber)]++
+	}
+
+	for i := 1; i < k; i++ {
+		c[i] += c[i-1]
+	}
+
+	for j := len(numbers) - 1; j >= 0; j-- {
+		res[c[digit(numbers[j], digitNumber)]-1] = numbers[j]
+		c[digit(numbers[j], digitNumber)]--
+	}
+
+	return res
+}
+
+func digit(number int, digitNumber int) int {
+	res := (float64(number) / (math.Pow(float64(10), float64(digitNumber-1))))
+	return int(res) % 10
 }
--- sortings/counting_by_digit_number_test.go
+++ sortings/counting_by_digit_number_test.go
@@ -15,8 +15,8 @@ var countingTestSet = []struct {
 	{[]int{}, []int{}, 0, 0},
 	{[]int{1}, []int{1}, 1, 1},
 	{[]int{2, 1}, []int{1, 2}, 2, 1},
-	{[]int{20, 10}, []int{10, 20}, 0, 1},
-	{[]int{20, 10}, []int{20, 10}, 2, 2},
+	{[]int{20, 10}, []int{20, 10}, 0, 1},
+	{[]int{20, 10}, []int{10, 20}, 2, 2},
 	{[]int{70, 60, 50, 40, 30, 20, 10}, []int{10, 20, 30, 40, 50, 60, 70}, 7, 2},
 	{[]int{127, 126, 125, 124, 123, 122, 121}, []int{121, 122, 123, 124, 125, 126, 127}, 7, 1},
 	{[]int{171, 162, 153, 144, 135, 126, 117}, []int{117, 126, 135, 144, 153, 162, 171}, 7, 2},
@@ -25,11 +25,11 @@ var countingTestSet = []struct {
 
 func TestCountingByDigitNumber(t *testing.T) {
 	for _, tt := range countingTestSet {
-		sortings.CountingByDigitNumber(tt.in, tt.maxNumber, tt.digitNumber)
+		got := sortings.CountingByDigitNumber(tt.in, tt.maxNumber, tt.digitNumber)
 
-		if !reflect.DeepEqual(tt.in, tt.out) {
+		if !reflect.DeepEqual(got, tt.out) {
 			t.Errorf("CountingByDigitNumber(%v, %v), got %v, want %v",
-				tt.maxNumber, tt.digitNumber, tt.in, tt.out)
+				tt.maxNumber, tt.digitNumber, got, tt.out)
 		}
 	}
 }
